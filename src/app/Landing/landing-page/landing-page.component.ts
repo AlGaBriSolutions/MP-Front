e7ed249fc9f1523e0fa757a876ad8945169cb278
alejo190404 <alejo190404@gmail.com>
2024-09-30 08:45:23 -0500

    Posibles cambios para Landing Page

--- src/app/Landing/landing-page/landing-page.component.ts
+++ src/app/Landing/landing-page/landing-page.component.ts
@@ -10,8 +10,6 @@ import { AfterViewInit, Component } from '@angular/core';
 export class LandingPageComponent implements AfterViewInit {
   
   ngOnInit(){
-    const html = document.querySelector('html') as HTMLElement;
-    html.setAttribute("style", "background-image: url(https://i.ibb.co/jrRb11q/img2.jpg); background-size: cover;");
     
   }
 
@@ -20,30 +18,10 @@ export class LandingPageComponent implements AfterViewInit {
   }
 
   next() {
-    const html = document.querySelector('html') as HTMLElement;
-    const slide = document.querySelector('.slide') as HTMLElement;
-    const items = slide.querySelectorAll('.item');
-    if (items.length > 0) {
-      const url:String = items[2].outerHTML;
-      slide.appendChild(items[0]);
-      
-      console.log(url.split("(")[1].split(")")[0]);
-      html.setAttribute("style", "background-image: url(" + url.split("(")[1].split(")")[0] + "); background-size: cover;");
-      
-      console.log(slide);
-    }
+    
   }
   
   prev() {
-    const html = document.querySelector('html') as HTMLElement;
-    const slide = document.querySelector('.slide') as HTMLElement;
-    const items = slide.querySelectorAll('.item');
-    if (items.length > 0) {
-      const url:String = items[0].outerHTML;
-      slide.prepend(items[items.length - 1]);
-
-      console.log(url.split("(")[1].split(")")[0]);
-      html.setAttribute("style", "background-image: url(" + url.split("(")[1].split(")")[0] + "); background-size: cover;");
-    }
+    
   }
 }

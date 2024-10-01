2e87f92033c5617e03ff704d0bf6a7d4d177f56b
J-Brinez <juanf.brinez@gmail.com>
2024-09-30 23:02:38 -0500

    Nueva landing funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './Landing/landing-page/landing-page.component';
+import { LandingSliderComponent } from './landing-slider/landing-slider.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: LandingPageComponent },
+  { path: '', pathMatch: 'full', component: LandingSliderComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './Landing/landing-page/landing-page.component';
+import { LandingSliderComponent } from './landing-slider/landing-slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    LandingSliderComponent
   ],
   imports: [
     BrowserModule,
